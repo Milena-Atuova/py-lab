71c5d13ecda220ffe57c9df85e6b369a3f45160f
Milena-Atuova <Atuovamilena@mail.ru>
2025-09-16 15:44:25 +0300

    1234

--- hh.py
+++
@@ -1,3 +0,0 @@
-print("esiufh")
-print(123)
-print(123456789)
--- lab01/src/01_greeting.py
+++ lab01/src/01_greeting.py
@@ -1,3 +1,3 @@
 name = input("Имя: ")
-age = int(input("Возраст: "))
+age = int(input("Возст: "))
 print(f"Привет, {name}! Через год тебе будет {age + 1}.")
