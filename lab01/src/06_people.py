9ea833a423702fc48f51eac816d4bbf7c80f9b3e
Milena-Atuova <Atuovamilena@mail.ru>
2025-09-17 23:32:44 +0300

    6

---
+++ lab01/src/06_people.py
@@ -0,0 +1,9 @@
+n=int(input("Количество студентов: "))
+och=zoch=0
+for i in range(n):
+    inf=input("Фамилия, Имя, возраст, форма обучения: ").split()
+    if inf[3]=="True":
+        och+=1
+    else:
+        zoch+=1
+print(och,zoch)
