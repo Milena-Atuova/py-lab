17ef0ad91bc4f9a19d081e87773b8ba225a0fa6a
Milena-Atuova <Atuovamilena@mail.ru>
2025-10-02 11:18:37 +0300

    2

--- src/lab2/01_arrays.py
+++ src/lab2/01_arrays.py
@@ -13,4 +13,15 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
 
     return tuple([mn, mx])
 
-print(min_max([1.5, 2, 2.0, -3.1]))
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+    return sorted(set(nums))
+
+
+def flatten(mat: list[list | tuple]) -> list:
+    array = list()
+    for arr in mat:
+        if not (isinstance(arr, tuple) or isinstance(arr, list)):
+            return TypeError
+        for member in arr:
+            array.append(member)
+    return array
